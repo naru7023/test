2d9dde28dbff376784c17d1f7cde30afde777292
naru7023 <104113307+naru7023@users.noreply.github.com>
2022-04-28 17:35:26 +0900

    220428-2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  console.log(li);
 };
 
 document
